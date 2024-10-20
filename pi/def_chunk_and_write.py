23ec4dc10a64548a532ad411aae2dc4b97235ca6
JackyMango <jackyzhao2004@gmail.com>
2024-10-20 13:33:28 -0400

    Updated Reader and Writer

---
+++ pi/def_chunk_and_write.py
@@ -0,0 +1,29 @@
+# data accumulation for sensors & microcontrollers [ALPHA]
+# Alex Lee
+# 10/1/2024
+
+import multiprocessing
+from multiprocessing import shared_memory
+import numpy as np
+import time
+import fileinput
+
+def writer(shm_name): 
+    try:
+        while True:
+            existing_shm = shared_memory.SharedMemory(name=shm_name)
+            # store array buffer from shm to array
+            arr = np.ndarray((10,), dtype=np.int16, buffer=existing_shm.buf)
+            data = int(input("Please input data: "))
+            arrayIndex = int(input("Input index 0-9: "))
+            arr[arrayIndex] = data
+            existing_shm.close()
+    except KeyboardInterrupt:
+        print("Closing Writer...")
+        
+    return 1
+
+# data accumulator program MUST BE RUNNING for this to work
+if __name__ == '__main__':
+    writer("mem123")
+    
