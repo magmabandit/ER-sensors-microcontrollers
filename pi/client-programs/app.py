0e241441d0d115eac71f3c7aa062e96c7754cd8a
helena-f <43306951+helena-f@users.noreply.github.com>
2025-01-26 14:03:49 -0500

    working flask app with shared memory read write access

---
+++ pi/client-programs/app.py
@@ -0,0 +1,33 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+from multiprocessing import shared_memory
+import time
+import atexit
+import serial
+#from ER-sensors-microcontrollers.pi.globals import *
+import numpy as np
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    existing_shm = shared_memory.SharedMemory(name="mem123")
+    arr = np.ndarray(shape=10, dtype=np.int64, buffer=existing_shm.buf)
+    # Convert array to string for display
+    arr_string = str(arr.tolist())
+    existing_shm.close()
+    return f'Array contents: {arr_string}'
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
