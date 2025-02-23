2478245b6cca4a193642ff34bc84e93213809692
JackyMango <jackyzhao2004@gmail.com>
2025-02-23 12:23:04 -0500

    Real-time updates for shared-memory using WebsocketsIO

--- pi/client-programs/app.py
+++ pi/client-programs/app.py
@@ -1,6 +1,8 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+import threading
 from multiprocessing import shared_memory
 import time
 import atexit
@@ -11,27 +13,48 @@ import numpy as np
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Reads the shared memory and creates an array called data that contains the
+# first 4 elements of the shared memory array.
+def read_shm():
+    try:
+        existing_shm = shared_memory.SharedMemory(name="mem123")
+        arr = np.ndarray(shape=10, dtype=np.float32, buffer=existing_shm.buf)
+        # Convert array to string for display
+        data = [arr[i].tolist() for i  in range(4)]
+        existing_shm.close()
+
+        return data
+    except FileNotFoundError:
+        return ["Error: Read from Shared Memory Failed."]
+
+# Emits the signal 'update_data' to the index.html file to run the javascript
+# code in the file.
+def background_thread():
+    while True:
+        shm_data = read_shm()
+        socketio.emit('update_data', {'array_data': shm_data})
+        time.sleep(1)
+
+# Creates a background thread that continously updates the display with
+# updated data from the shared memory.
+thread = threading.Thread(target = background_thread, daemon = True)
+thread.start()
 
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
+# ‘/’ URL is bound with hello_world() function. 
 def hello_world():
-    existing_shm = shared_memory.SharedMemory(name="mem123")
-    arr = np.ndarray(shape=10, dtype=np.int64, buffer=existing_shm.buf)
-    # Convert array to string for display
-    arr_string0 = str(arr[0].tolist())
-    arr_string1 = str(arr[1].tolist())
-    arr_string2 = str(arr[2].tolist())
-    arr_string3 = str(arr[3].tolist())
-    
-    existing_shm.close()
-    return render_template("index.html", array_data = [arr_string0, arr_string1, arr_string2, arr_string3])
+
+    shm_data = read_shm()
+    return render_template("index.html", array_data = shm_data)
 
 # main driver function
 if __name__ == '__main__':
 
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+    # Runs the app using socketio for real-time data updates from the
+    # shared memory.
+    socketio.run(app, debug = True)
